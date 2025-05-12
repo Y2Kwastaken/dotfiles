70c1a960a4388bfc4e117d89b5bb0aa76d31f447
Y2Kwastaken <mwholder@proton.me>
2025-05-12 11:38:13 -0500

    Initial Commit

---
+++ deploy_dotfiles.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+SCRIPT_DIR="$(dirname -- "$0")"
+DEPLOY=$1
+
+if test -z "$DEPLOY"; then
+    echo "Deploying All Dot Files to ~/"
+    cp -r $SCRIPT_DIR ~/
+    rm ~/deploy_dotfiles.sh
+else
+    echo "Deploying Subdirectory $SCRIPT_DIR/$DEPLOY to ~/../$DEPLOY"
+    cp -r $SCRIPT_DIR/$DEPLOY ~/$DEPLOY/../
+fi
