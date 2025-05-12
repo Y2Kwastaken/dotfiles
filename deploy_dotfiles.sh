a1fd05b1b6cd0331c31a66aac4c3b8f89728b0a4
Y2Kwastaken <mwholder@proton.me>
2025-05-12 14:54:02 -0500

    some more tweaks

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -5,6 +5,7 @@ DEPLOY=$1
 if test -z "$DEPLOY"; then
     echo "Deploying All Dot Files to ~/"
     cp -r $SCRIPT_DIR ~/
+    rm -rf ~/.git
     rm ~/deploy_dotfiles.sh
 else
     echo "Deploying Subdirectory $SCRIPT_DIR/$DEPLOY to ~/../$DEPLOY"
